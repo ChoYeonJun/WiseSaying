07c20543ded713c8efbe61bc61a3fb618d26f980
YeonJun Cho <micayel1219@gmail.com>
2022-07-14 22:04:57 +0900

    add Hashmap on Rq class

--- src/main/java/com/example/wisesaying/Rq.java
+++ src/main/java/com/example/wisesaying/Rq.java
@@ -1,38 +1,59 @@
 package com.example.wisesaying;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rq {
     String url;
     String path;
-    String queryStr;
+
+    Map<String, String> queryParams;
+
+    /**
+     * String url
+     * C - 등록
+     * R - 목록
+     * U - ??
+     * D - 삭제?id=1
+     */
 
     public Rq(String url) {
         this.url = url;
         String[] urlBits = url.split("\\?", 2);
         this.path = urlBits[0];
 
+        queryParams = new HashMap<>();
+
         if(urlBits.length == 2){
-            this.queryStr = urlBits[1];
+            String queryStr = urlBits[1]; // id=1 or id=10&no=1
+
+            String[] paramBits = queryStr.split("&"); //  id=10 / no=1
+
+            for(String paramBit : paramBits){
+                String[] paramNameAndValue = paramBit.split("=", 2);
+
+                if(paramNameAndValue.length == 1)
+                    continue;
+
+
+                String paramName = paramNameAndValue[0].trim(); //id, no
+                String paramValue = paramNameAndValue[1].trim(); // 10, 1
+
+                queryParams.put(paramName, paramValue);
+            }
         }
     }
 
     public int getIntParam(String paramName, int defaultValue) {
-        if(queryStr == null){
+        if(queryParams.containsKey(paramName)){
             return defaultValue;
         }
+        String paramValue = queryParams.get(paramName);
 
-        String[] bits = queryStr.split("&");
+        if(paramValue.length() == 0)
+            return defaultValue;
 
-        for (String urlBit : bits) {
-            String[] paramNameAndValue = urlBit.split("=", 2);
-            String paramName_ = paramNameAndValue[0];
-            String paramValue = paramNameAndValue[1];
-
-            if (paramName.equals(paramName_)) {
-                return Integer.parseInt(paramValue);
-            }
-        }
-
-        return defaultValue;
+        return Integer.parseInt(paramValue);
     }
 
     public String getPath() {
