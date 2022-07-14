1addb0ca6639ce1ee3e7e2e488978fac6b521ddc
YeonJun Cho <micayel1219@gmail.com>
2022-07-14 17:15:37 +0900

    get Int Param test 2 생성

--- src/test/java/com/example/wisesaying/AppTest.java
+++ src/test/java/com/example/wisesaying/AppTest.java
@@ -60,4 +60,15 @@ public class AppTest {
         //then
         assertEquals(1, id);
     }
+
+    @Test
+    public void Rq__getIntParam_2(){
+        Rq rq = new Rq("검색?id=10&no=1");
+
+        int id = rq.getIntParam("id", 0);
+        int no = rq.getIntParam("no", 0);
+
+        assertEquals(10, id);
+        assertEquals(1, no);
+    }
 }
