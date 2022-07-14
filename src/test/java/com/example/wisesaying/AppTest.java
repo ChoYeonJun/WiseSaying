8c40f4ea5a3ba97ceb688e9927c82c0846a4432e
YeonJun Cho <micayel1219@gmail.com>
2022-07-14 17:17:37 +0900

    get path test add

--- src/test/java/com/example/wisesaying/AppTest.java
+++ src/test/java/com/example/wisesaying/AppTest.java
@@ -71,4 +71,11 @@ public class AppTest {
         assertEquals(10, id);
         assertEquals(1, no);
     }
+
+    @Test
+    public void Rq__getPath(){
+        Rq rq = new Rq("삭제?id=1");
+        String path = rq.getPath();
+        assertEquals("삭제", path);
+    }
 }
