b7315ed18590fbed6e25aad045439dfbcdbede2c
YeonJun Cho <micayel1219@gmail.com>
2022-07-14 17:03:36 +0900

    scanner test add

--- src/test/java/com/example/wisesaying/AppTest.java
+++ src/test/java/com/example/wisesaying/AppTest.java
@@ -2,6 +2,10 @@ package com.example.wisesaying;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
@@ -9,5 +13,25 @@ public class AppTest {
     public void 테스트_실험() {
         int rs = 10 + 20;
         assertEquals(30, rs);
+    }
+
+    @Test
+    public void 테스트_스캐너(){
+        String input = """
+                등록
+                명언 1
+                작가 1
+                """.stripIndent();
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        Scanner sc = new Scanner(in);
+
+        String cmd1 = sc.nextLine().trim();
+        String cmd2 = sc.nextLine().trim();
+        String cmd3 = sc.nextLine().trim();
+
+        assertEquals("등록", cmd1);
+        assertEquals("명언 1", cmd2);
+        assertEquals("작가 1", cmd3);
     }
 }
